b2f138ac3ef8321f98fb4a4d1a5fcbb24bdf4149
Tom Flídr <tomflidr@gmail.com>
2017-01-23 22:35:54 +0100

    base commit

---
+++ make-phar.php
@@ -0,0 +1,17 @@
+<?php
+
+	// include Packager library by composer autoload (you can do it only for PHAR packing):
+	include_once('vendor/autoload.php');
+	
+
+	// include basic configuration for PHAR packing:
+	include_once('.packager/config-phar-with-composer.php');
+
+	
+	Packager_Phar::Create($config)
+	
+		// print files to pack by previous $config array and exit:
+		//->PrintFilesToPack()
+		
+		// run packing multiprocess:
+		->Run();
